10ce1b0a210d009d2ffd61e24a843544c020d57d
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-09-29 10:13:27 +0200

    Class 138

--- controllers/user.js
+++ controllers/user.js
@@ -12,6 +12,28 @@ var controller = {
     return res.status(200).send({
       message: "Soy el método testeando"
     });
+  },
+
+  save: function(req, res) {
+    // Recoger los parámetros de la petición
+
+    // Validar los datos
+
+    // Crear objeto de usuario
+
+    // Asignar valores al usuario
+
+    // Comprobar si el usuario ya existe
+
+    // Si no existe, cifrar la contraseña
+    
+    // Si no existe, guardarlo
+
+    // Devolver respuesta
+
+    return res.status(200).send({
+      message: "Registro de usuarios"
+    });
   }
 
 };
