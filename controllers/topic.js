62208a7d1d208450c66f50767c4e01b0f8ca1aa9
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-01 15:46:23 +0200

    Class 159

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,5 +1,8 @@
 'use strict'
 
+var validator = require('validator');
+var Topic = require('../models/topic');
+
 var controller = {
 
   test: function(req, res) {
@@ -10,19 +13,50 @@ var controller = {
 
   save: function(req, res) {
     // Recoger los parámetros de la petición
+    var params = req.body;
 
     // Validar los datos
+    try {
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_content = !validator.isEmpty(params.content);
+      var validate_lang = !validator.isEmpty(params.lang);
+    } catch(err) {
+      return res.status(400).send({
+        message: "Faltan datos por enviar"
+      });
+    }
 
-    // Crear objeto a guardar
+    if (validate_title && validate_content && validate_lang) {
+      // Crear objeto de topic
+      var topic = new Topic();
+      
+      // Asignar valores
+      topic.title = params.title;
+      topic.content = params.content;
+      topic.code = params.code;
+      topic.lang = params.lang;
+      
+      // Guardar el topic
+      topic.save((err, topicStored) => {
+        // Devolver respuesta
+        if (err || !topicStored) {
+          return res.status(400).send({
+            status: "error",
+            message: "El tema no se ha guardado"
+          });
+        }
 
-    // Asignar valores
+        return res.status(200).send({
+          status: "success",
+          topic: topicStored
+        });
+      });
 
-    // Guardar el topic
-
-    // Devolver respuesta
-    return res.status(200).send({
-      message: "Soy el método save"
-    });
+    } else {
+      return res.status(400).send({
+        message: "Los datos no son válidos"
+      });
+    }
   }
 
 }
