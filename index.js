b96fff7b7d48459ebbd330142854a932ef98568f
Rafael Díaz García <diaz.garcia.rafa@gmail.com>
2020-10-01 10:02:22 +0200

    Class 151

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = require('./app');
 var port = process.env.port || 3999;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .then(() => {
     console.log('La conexión a la base de datos de mongo se ha realizado correctamente.')
 
